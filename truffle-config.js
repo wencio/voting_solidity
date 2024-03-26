97f473201fe82dd82a3330459f77b39fa8c658ac
wencio daniel martinez <97320103+wencio@users.noreply.github.com>
2024-03-25 20:11:14 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   compilers: {
     solc: {
-      version: '0.5.2'
+      version: '0.8.0'
     }
   }
 };
